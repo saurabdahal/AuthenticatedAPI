59cc5431f7f0745fd71631bbd1463e001c9b1b83
saurav dahal <d.saurab1@gmail.com>
2024-02-21 18:11:03 -0500

    feat: Created Category model and added Category reference in Product model

--- AuthentationWebAPI/Data/Product.cs
+++ AuthentationWebAPI/Data/Product.cs
@@ -6,5 +6,8 @@
         public decimal Price { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        // Add a Category property
+        public Category ProductCategory { get; set; }
     }
 }
